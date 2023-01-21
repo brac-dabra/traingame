b13dda75f0a66fa1259f0e3f19cf7d1154bf48e3
arba <abracadabra.arba@gmail.com>
2023-01-21 17:39:07 +0000

    Strech all levels to fit the screen better, fix rails alignment and brown color in level 10

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674318580|21610801389";
+const CACHE_VERSION = "1674322649|3366225883";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
