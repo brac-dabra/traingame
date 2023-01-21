e83f600ff1aa57e9179a7c4fa8eee9515c122f9b
arba <abracadabra.arba@gmail.com>
2023-01-21 16:25:40 +0000

    Add level 10, change rail path spacing and some cosmetics

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1673780686|2197386341";
+const CACHE_VERSION = "1674318115|21146138237";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
