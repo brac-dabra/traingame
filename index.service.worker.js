637d31e4e240a16c7f5965d89c18856e6b1ae698
arba <abracadabra.arba@gmail.com>
2023-02-05 21:43:24 +0000

    Apply fixes on many levels. Extend the usage of the SignalHub.

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1675603018|2054345019";
+const CACHE_VERSION = "1675633333|4927866559";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
