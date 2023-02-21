fbdc56ca653e3f18be80151970dc70d0f9fb0759
arba <abracadabra.arba@gmail.com>
2023-02-21 12:04:18 +0000

    Add Turbo Meter and limit power usage on levels

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1676235088|506987508";
+const CACHE_VERSION = "1676981001|12082828992";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
