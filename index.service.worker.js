184e22172cf3fdb82870da507f2277682c4abe45
arba <abracadabra.arba@gmail.com>
2023-02-22 10:45:30 +0000

    add tchou tchou sound

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1676981001|12082828992";
+const CACHE_VERSION = "1677062622|5304513738";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
