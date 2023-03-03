e5e094eddfac0dab60104d89683c8ecd1833d909
arba <abracadabra.arba@gmail.com>
2023-03-03 23:48:19 +0000

    Cosmetic changes. Remove lights and shadows. Add decoration to levels

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1677705092|6034722725";
+const CACHE_VERSION = "1677887241|19169876337";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
