c9f3e4b0b14368ef46df96ea32badb36244597d3
arba <abracadabra.arba@gmail.com>
2023-03-18 10:13:55 +0000

    New visual for tunnel. Difficulty adjustments.

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1678567592|177671505";
+const CACHE_VERSION = "1679134367|7389111544";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
