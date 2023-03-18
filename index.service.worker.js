4a2e5ddc79f11f26933d767194ac15251cdaa6a2
arba <abracadabra.arba@gmail.com>
2023-03-18 18:59:35 +0000

    Change visuals for tunnel, station and train

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1679134367|7389111544";
+const CACHE_VERSION = "1679165943|2240741515";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
