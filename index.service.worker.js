4c4e61c91871452e1ebd7072fa9d0e36bd22c36b
arba <abracadabra.arba@gmail.com>
2023-10-03 12:40:28 +0100

    Add better shadows to objects

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1680958179|2827460442";
+const CACHE_VERSION = "1696333130|2473256728";
 const CACHE_PREFIX = "Train Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
